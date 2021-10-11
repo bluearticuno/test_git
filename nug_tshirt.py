c046c3687889626ba029e2fe24f1c08cb617595b
nugnug <nur.cahyo.nugroho@gmail.com>
2021-10-11 22:49:28 +0800

    This is my first commit

--- nug_tshirt.py
+++ nug_tshirt.py
@@ -11,6 +11,7 @@ def regression(test_data, reg):
     print("Predicted from {} is {}.".format(test_data, reg.predict(test_data)))
 
 def main():
+    #Nugi added to test update to develop branch
     #Putting this code to LOCAL BRANCH
     #Error handling when creating the engine to SQLite
     try:
